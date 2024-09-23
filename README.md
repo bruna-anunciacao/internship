9c4bbfc39990284a0b73e46ded2746d9e5938fe6
bruna-anunciacao <bruunaaa04@gmail.com>
2024-09-23 14:50:31 -0300

    feat: finish style and start readme

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# React + Vite
+# Desafio Target Sistemas
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Nesse repositório foram realizadas as soluções dos desafios propostos no processo seletivo. A linguagem utilizada foi Javascript.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import './App.css'
-import Fibonacci from './components/Fibonacci/Fibonacci'
-import LetterRecurrence from './components/LetterRecurrence/LetterRecurrence'
-import Lights from './components/Lights/Lights'
-import NextElement from './components/NextElement/NextElement'
-import SumCode from './components/SumCode/SumCode'
+// Components
+import Fibonacci from "./components/Fibonacci/Fibonacci";
+import LetterRecurrence from "./components/LetterRecurrence/LetterRecurrence";
+import Lights from "./components/Lights/Lights";
+import NextElement from "./components/NextElement/NextElement";
+import SumCode from "./components/SumCode/SumCode";
+// Styles
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <Fibonacci />
-      <LetterRecurrence />
-      <SumCode />
-      <NextElement />
-      <Lights />
-    </>
-  )
+    <main className="mainWrapper">
+      <h1 className="mainTitle">Desafio Target Sistemas</h1>
+      <div className="card"><Fibonacci /></div>
+      <div className="card"><LetterRecurrence /></div>
+      <div className="card"><SumCode /></div>
+      <div className="card"><NextElement /></div>
+      <div className="card"><Lights /></div>
+    </main>
+  );
 }
 
-export default App
+export default App;
