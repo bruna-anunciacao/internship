f27f893e14750b377e2fa57f33560b03162daa43
bruna-anunciacao <bruunaaa04@gmail.com>
2024-09-23 14:21:19 -0300

    feat: finish next element function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Fibonacci from './components/Fibonacci/Fibonacci'
 import LetterRecurrence from './components/LetterRecurrence/LetterRecurrence'
+import NextElement from './components/NextElement/NextElement'
 import SumCode from './components/SumCode/SumCode'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Fibonacci />
       <LetterRecurrence />
       <SumCode />
+      <NextElement />
     </>
   )
 }
---
+++ src/components/NextElement/NextElement.jsx
@@ -0,0 +1,16 @@
+export default function NextElement() {
+  return (
+    <div>
+      <h1>Próximo elemento da sequência</h1>
+      <p>
+        4) Descubra a lógica e complete o próximo elemento: <br/>
+        a) 1, 3, 5, 7, <b>9</b> <br/>
+        b) 2, 4, 8, 16, 32, 64, <b>128</b> <br/>
+        c) 0, 1, 4, 9, 16, 25, 36, <b>49</b> <br/>
+        d) 4, 16, 36, 64, <b>100</b> <br/>
+        e) 1, 1, 2, 3, 5, 8, <b>13</b> <br/>
+        f) 2,10, 12, 16, 17, 18, 19, <b>20</b> <br/>
+      </p>
+    </div>
+  );
+}
